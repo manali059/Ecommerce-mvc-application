10a25340587cf1cc068b373c1386b5fa1d448949
Asus <Asus@DESKTOP-ASGAT10>
2023-01-13 12:51:21 +0530

    Add project files.

---
+++ Etickets1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Etickets1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
